453cdd8656794f8d367d9396802a01d10cb0d8a6
pachevar <lluviadeideaseditorial@gmail.com>
2026-04-03 11:01:53 -0600

    Fix: corregir version de vite y rutas de despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Base path for standard GitHub Pages: username.github.io/repo/
-  // Set to './' to allow the build to work from any relative path
-  base: './'
+  base: '/xelatactic/'
 })
